aa77698ff6e7e25ef28177b8d37631f51ea3717e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-06-18 23:37:42 +0900

    ShareDataクラスを定義して共有する変数をパブリッシュする

---
+++ SwiftUITextbookChapter7/SwiftUITextbookChapter7/Section4_ShareData.swift
@@ -0,0 +1,14 @@
+//
+//  Section4_ShareData.swift
+//  SwiftUITextbookChapter7
+//
+//  Created by home on 2020/06/18.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class ShareData: ObservableObject {
+    @Published var yesNo = false
+    @Published var num = 1
+}
